eedb94c86ee23dd7aa1590a4c2c8cdfc08215b0b
shivam21 <shivam91066@gmail.com>
2026-08-08 21:40:25 +0530

    feat: added rule caching mechanism logic

--- cache.go
+++ cache.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"sync"
 	"time"
 )
 
@@ -12,4 +14,37 @@ type Rule struct {
 type CachedRule struct {
 	Rule      Rule
 	ExpiresAt time.Time
+}
+
+type RuleCache struct {
+	mu    sync.RWMutex
+	store map[string]CachedRule
+}
+
+func NewRuleCache() *RuleCache {
+	return &RuleCache{
+		store: make(map[string]CachedRule),
+	}
+}
+
+// fetching rules from cache if present
+func (c *RuleCache) GetRule(db *sql.DB, userID string) (Rule, error) {
+	c.mu.RLock()
+	cached, found := c.store[userID]
+	if found && time.Now().Before(cached.ExpiresAt) {
+		c.mu.RUnlock()
+		return cached.Rule, nil
+	}
+	c.mu.RUnlock()
+
+	rule := Rule{Limit: 5, Window: 10 * time.Second} // for now using harcoded fallback rule instead querying PG for now
+
+	c.mu.Lock()
+	c.store[userID] = CachedRule{
+		Rule:      rule,
+		ExpiresAt: time.Now().Add(30 * time.Second),
+	}
+	c.mu.Unlock()
+
+	return rule, nil
 }
